d6e0a641a504efc437f38962f554edcd53ff9d21
Lane Russell <lane.russell@givecampus.com>
2024-12-15 18:44:27 -0700

    moves prompt to top in ivy theme for telescope

--- init.lua
+++ init.lua
@@ -344,11 +344,15 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          layout_config = {
+            height = 0.7, -- doesn't appear to actually work yet
+            prompt_position = 'top',
+          },
+          --   mappings = {
+          --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+          --   },
+        },
         pickers = {
           help_tags = { theme = 'ivy' },
           keymaps = { theme = 'ivy' },
