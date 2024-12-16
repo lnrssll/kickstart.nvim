041414b9a4552f8cccb3a8d27309dc3ace420861
Lane Russell <lane.russell@givecampus.com>
2024-12-15 18:11:10 -0700

    adds mini.jump for better navigation

--- init.lua
+++ init.lua
@@ -836,6 +836,9 @@ require('lazy').setup({
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
+      -- just extends f,F,t,T,;
+      require('mini.jump').setup { idle_stop = 5000 }
+
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
